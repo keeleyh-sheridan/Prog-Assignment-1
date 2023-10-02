f9c97b826d77e2cc59d6c44170b899723a25c794
Hugh K <hughrkeeley@gmail.com>
2023-10-02 08:14:17 -0400

    First challenge works

--- App.py
+++ App.py
@@ -7,4 +7,16 @@ print("\nWelcome to my game! \nYou play the part of a character deeply in debt, 
 selectionText = "\nChoose your role! \nThe gambler is determined to not quit before his big win, while the fool blindly stumbles into good luck. \n(gambler, fool): "
 while Game.choose_role(input(selectionText)):
     print()
-print(Game.player)
+
+print("\nYour stats are:\nWill: " + str(Game.get_will()) + "\nLuck: " + str(Game.get_luck()) + "\nBluff: " + str(Game.get_bluff()))
+
+print("Challenge 1")
+input("\nPress -Enter- to roll 2 dice and add your bluff stat (" + str(Game.get_bluff()) + "): ")
+difficulty = 7
+roll = Game.roll_dice(Game.get_bluff(), difficulty)
+print("You rolled a", roll, "you need at least a", difficulty)
+
+if roll >= difficulty:
+    print("Succeed")
+else:
+    print("Fail")
--- Game.py
+++ Game.py
@@ -17,3 +17,22 @@ def choose_role(role):
         run = False
 
     return run
+
+def get_will():
+    return player.will
+
+def get_luck():
+    return player.luck
+
+def get_bluff():
+    return player.bluff
+
+def roll_dice(stat, difficulty):
+    roll = random.randint(2,12) + stat
+
+    if roll < difficulty:
+        player.will -= 1
+    else:
+        player.will += 1
+
+    return roll
